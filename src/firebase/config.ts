17e51eda578a7c14771e472788f8265c132b18d3
Layla Astraea <laylaastraea@gmail.com>
2025-10-26 06:48:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5979032199-2e240",
+  "appId": "1:860501503529:web:0773831b559271682b3638",
+  "apiKey": "AIzaSyB3ui9Ew9le79F25C3M-niFvblUqcSI9_I",
+  "authDomain": "studio-5979032199-2e240.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "860501503529"
+};
